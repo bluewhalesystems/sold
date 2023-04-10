91607968570d4002aa953e92cdffa2787e14677e
Rui Ueyama <ruiu@bluewhale.systems>
2023-04-10 15:11:43 +0900

    Fix test

--- test/macho/export-dynamic.sh
+++ test/macho/export-dynamic.sh
@@ -15,10 +15,10 @@ EOF
 
 $CC --ld-path=./ld64 -o $t/exe1 $t/a.o -flto -Wl,-no_fixup_chains
 $t/exe1 | grep -q 'Hello world'
-nm -dyldinfo-only $t/exe1 > $t/log1
+nm -g $t/exe1 > $t/log1
 ! grep -q _hello $t/log1 || false
 
 $CC --ld-path=./ld64 -o $t/exe2 $t/a.o -flto -Wl,-no_fixup_chains -Wl,-export_dynamic
 $t/exe2 | grep -q 'Hello world'
-nm -dyldinfo-only $t/exe2 > $t/log2
+nm -g $t/exe2 > $t/log2
 grep -q _hello $t/log2
