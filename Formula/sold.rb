dd89a689d6173bdf8d0bcc7c3e9a895e881fddbc
Joel McKenzie <joelm@soundhound.com>
2024-10-04 08:29:07 +0200

    Add a brew tap file

---
+++ Formula/sold.rb
@@ -0,0 +1,109 @@
+class Sold < Formula
+  desc "Modern Linker"
+  homepage "https://github.com/bluewhalesystems/sold.git"
+  head 'git@github.com:jmcken8/sold.git', :using => :git, :branch => "main"
+  license "MIT"
+
+
+  depends_on "cmake" => :build
+  depends_on "blake3"
+  depends_on "tbb"
+  depends_on "zstd"
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1200
+  end
+
+  on_linux do
+    depends_on "mimalloc"
+  end
+
+  fails_with :clang do
+    build 1200
+    cause "Requires C++20"
+  end
+
+  fails_with :gcc do
+    version "7"
+    cause "Requires C++20"
+  end
+
+  def install
+    ENV.llvm_clang if OS.mac? && (DevelopmentTools.clang_build_version <= 1200)
+
+    # Avoid embedding libdir in the binary.
+    # This helps make the bottle relocatable.
+    inreplace "common/config.h.in", "@CMAKE_INSTALL_FULL_LIBDIR@", ""
+    # Ensure we're using Homebrew-provided versions of these dependencies.
+    %w[blake3 mimalloc tbb zlib zstd].each { |dir| rm_r(HOMEBREW_PREFIX/"Cellar"/dir) }
+    args = %w[
+      -DMOLD_LTO=ON
+      -DMOLD_USE_MIMALLOC=ON
+      -DMOLD_USE_SYSTEM_MIMALLOC=ON
+      -DMOLD_USE_SYSTEM_TBB=ON
+      -DCMAKE_SKIP_INSTALL_RULES=OFF
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    pkgshare.install "test"
+  end
+
+  def caveats
+    <<~EOS
+      Support for Mach-O targets has been removed.
+      See https://github.com/bluewhalesystems/sold for macOS/iOS support.
+    EOS
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      int main(void) { return 0; }
+    EOS
+
+    linker_flag = case ENV.compiler
+    when /^gcc(-(\d|10|11))?$/ then "-B#{libexec}/mold"
+    when :clang, /^gcc-\d{2,}$/ then "-fuse-ld=mold"
+    else odie "unexpected compiler"
+    end
+
+    extra_flags = %w[-fPIE -pie]
+    extra_flags += %w[--target=x86_64-unknown-linux-gnu -nostdlib] unless OS.linux?
+
+    system ENV.cc, linker_flag, *extra_flags, "test.c"
+    if OS.linux?
+      system "./a.out"
+    else
+      assert_match "ELF 64-bit LSB pie executable, x86-64", shell_output("file a.out")
+    end
+
+    return unless OS.linux?
+
+    cp_r pkgshare/"test", testpath
+
+    # Remove non-native tests.
+    arch = Hardware::CPU.arm? ? "aarch64" : Hardware::CPU.arch.to_s
+    testpath.glob("test/elf/*.sh")
+            .reject { |f| f.basename(".sh").to_s.match?(/^(#{arch}_)?[^_]+$/) }
+            .each(&:unlink)
+
+    inreplace testpath.glob("test/elf/*.sh") do |s|
+      s.gsub!(%r{(\./|`pwd`/)?mold-wrapper}, lib/"mold/mold-wrapper", audit_result: false)
+      s.gsub!(%r{(\.|`pwd`)/mold}, bin/"mold", audit_result: false)
+      s.gsub!(/-B(\.|`pwd`)/, "-B#{libexec}/mold", audit_result: false)
+    end
+
+    # The `inreplace` rules above do not work well on this test. To avoid adding
+    # too much complexity to the regex rules, it is manually tested below
+    # instead.
+    (testpath/"test/elf/mold-wrapper2.sh").unlink
+    assert_match "mold-wrapper.so",
+      shell_output("#{bin}/mold -run bash -c 'echo $LD_PRELOAD'")
+
+    # Run the remaining tests.
+    testpath.glob("test/elf/*.sh").each { |t| system "bash", t }
+  end
+end
